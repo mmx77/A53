aed83de49ae8777dd5535fd6af8d018f8c7dd679
luisjosegil <luisjosegil@gmail.com>
2017-10-16 17:12:31 +0200

    Update miner.sh

--- miner.sh
+++ miner.sh
@@ -12,10 +12,9 @@ else
         echo "Miner process $MINER_PID killed"
 fi
 
-./TEST/m-minerd_A53 -c ./cfg.json -B > /tmp/log.miner 2>&1
-#/usr/bin/nice -n19 ~/LABO/Miner/TEST/m-minerd_A53 -c ~/LABO/Miner/cfg.json -B > /tmp/log.miner 2>&1
-#/usr/bin/nice -n19 ~/LABO/Miner/TEST/m-minerd_O3 -c ~/LABO/Miner/cfg.json -B > /tmp/log.miner 2>&1
-
+cd TEST
+./m-minerd_A53 -c ../cfg.json -B > /tmp/log.miner 2>&1
+cd ..
 
 MINER_PID=$(/bin/ps -ef | /bin/grep $MINER_NAME | /bin/grep -v grep | awk '{ print $2 }')
 echo "new $MINER_NAME instance running with PID $MINER_PID"
