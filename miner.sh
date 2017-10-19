21d0984a108017167fd068a7c15e21bbd65b97e3
luisjosegil <luisjosegil@gmail.com>
2017-10-19 17:19:58 +0200

    Update miner.sh

--- miner.sh
+++ miner.sh
@@ -2,8 +2,9 @@
 # GLOBAL VARIABLES ----------------
 source ./config.control
 
+# MAIN CODE -----------------------
+set -x
 
-# First, kill miner if exists
 if (($CPU_TYPE==$CPU_A7));
 then
         MINER_NAME=$(echo "$MINER_A53")
@@ -11,7 +12,8 @@ elif (($CPU_TYPE==$CPU_A53));
         MINER_NAME=$(echo "$MINER_A7")
 fi
 
-MINER_NAME="minerd"
+# Kill miner if exists
+#MINER_NAME="minerd"
 MINER_PID=$(/bin/ps -ef | /bin/grep $MINER_NAME | /bin/grep -v grep | awk '{ print $2 }')
 
 if [ -z $MINER_PID ]
