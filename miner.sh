bb8e786c99ae0d95f8e16658e44d20a55ba4d7dd
luisjosegil <luisjosegil@gmail.com>
2017-10-19 14:55:21 +0200

    Update miner.sh

--- miner.sh
+++ miner.sh
@@ -1,5 +1,15 @@
 #!/bin/bash
+# Load config
+
+
 # First, kill miner if exists
+if (($CPU_TYPE==$CPU_A7));
+then
+        MINER_NAME=$(echo "$MINER_A53")
+elif (($CPU_TYPE==$CPU_A53));
+        MINER_NAME=$(echo "$MINER_A7")
+fi
+
 MINER_NAME="minerd"
 MINER_PID=$(/bin/ps -ef | /bin/grep $MINER_NAME | /bin/grep -v grep | awk '{ print $2 }')
 
@@ -13,7 +23,7 @@ else
 fi
 
 cd TEST
-./m-minerd_A53 -c ../cfg.json -B > /tmp/log.miner 2>&1
+./$MINERNAME -c ../cfg.json -B > /tmp/log.miner 2>&1
 cd ..
 
 MINER_PID=$(/bin/ps -ef | /bin/grep $MINER_NAME | /bin/grep -v grep | awk '{ print $2 }')
