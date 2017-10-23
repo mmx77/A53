cb218bb28add65a61adbfd09b56c34b071c4df0c
luisjosegil <luisjosegil@gmail.com>
2017-10-23 11:20:50 +0200

    Update miner.sh

--- miner.sh
+++ miner.sh
@@ -26,32 +26,36 @@ function retrieve_HW_processor {
 }
 
 function retrieve_compiled_name {
-	PROCESSOR=$(retrieve_HW_processor)
-	if [ "$PROCESSOR" == "$MNR_CPU_HW_A7" ];
-	then
-        	echo "$MNR_COMPILED_A7"
-	elif [ "$PROCESSOR" == "$MNR_CPU_HW_A53" ];
-	then
-        	echo "$MNR_COMPILED_A53"
-	fi
+	case $(retrieve_HW_processor) in
+		"$MNR_CPU_HW_A7")
+			echo "$MNR_COMPILED_A7"
+			;;
+		"$MNR_CPU_HW_A53")
+        		echo "$MNR_COMPILED_A53"
+			;;
+	esac
 }
 
 function update_config_file {
-	#local NEW_CONFIG_LINE=$(echo '"'"$1"'"="'"$2"'"' )
 	local OLD_CONFIG_LINE=$(echo "$1=")
 	local NEW_CONFIG_LINE=$(echo "$1="'"'"$2"'"' )
-	sed -i "/$OLD_CONFIG_LINE/c\\$NEW_CONFIG_LINE" "$CONFIG_FILE_SCRIPTS"
+	CUR_CONFIG_LINE=$(grep "$OLD_CONFIG_LINE" "$CONFIG_FILE_SCRIPTS" )
+
+	# Update config file, if not updated yet
+	if [[ "$CUR_CONFIG_LINE" != "$NEW_CONFIG_LINE" ]];
+	then
+		sed -i "/$OLD_CONFIG_LINE/c\\$NEW_CONFIG_LINE" "$CONFIG_FILE_SCRIPTS"
+	fi
 }
 
 # MAIN CODE -----------------------
 #set -x
 
 MNR_NAME=$(retrieve_compiled_name)
-# Update config file for the rest of scripts
+# Update config file for the rest of scripts, if necessary
 update_config_file $MNR_LABEL_NAME_OF_MINER $MNR_NAME
 
 # Kill miner if exists
-#MINER_NAME="minerd"
 MNR_PID=$(get_PID_by_name $MNR_NAME)
 kill_process_by_PID $MNR_PID
 
